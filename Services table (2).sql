ea794fd09157ea37aa528222456b7666e87a1b1e
Bagorogile-Mphaka <103036064+Bagorogile-Mphaka@users.noreply.github.com>
2025-09-06 22:45:39 +0200

    Add files via upload

---
+++ Services table (2).sql
@@ -0,0 +1,5 @@
+Create Table Services
+( ServiceId int Not Null Primary Key,
+Name nvarchar(50) Not Null,
+Price int Not Null
+)
---
+++ Stylist table.sql
@@ -0,0 +1,8 @@
+Create Table Stylist
+(StylistId  int not null Primary Key,
+FirstName nvarchar (50) Not Null,
+LastName nvarchar (50) Not Null,
+PhoneNumber nvarchar (10) Not Null,
+Email nvarchar (100) Not Null,
+)
+
