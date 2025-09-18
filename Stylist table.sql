940bdbbb2635746319bd02db9876f6d5b4e56312
Bagorogile-Mphaka <103036064+Bagorogile-Mphaka@users.noreply.github.com>
2025-09-18 04:45:17 +0200

    Delete Stylist table.sql

--- Stylist table.sql
+++
@@ -1,8 +0,0 @@
-Create Table Stylist
-(StylistId  int not null Primary Key,
-FirstName nvarchar (50) Not Null,
-LastName nvarchar (50) Not Null,
-PhoneNumber nvarchar (10) Not Null,
-Email nvarchar (100) Not Null,
-)
-
